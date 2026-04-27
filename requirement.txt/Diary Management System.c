b2b2211f326edfa77301bd21316ffba03b738388
raiyannafeem-cse <raiyannafeem@gmail.com>
2026-04-28 00:44:03 +0600

    Update Diary Management System.c
    updated the password and made it more stronger

--- requirement.txt/Diary Management System.c
+++ requirement.txt/Diary Management System.c
@@ -5,25 +5,43 @@
 int main()
 {
     int choice;
-    char date[15],location[50],subject[100],description[10000],search[50],line[15000],input_pass[70];
-    FILE *fp;
+    char date[15],location[50],subject[100],description[10000],search[50],line[15000],input_pass[70],pass[70];
+    FILE *fp ,*password;
 
-  for(int i=1;i<=3;i++){ // MAXIMUM 3 ATTEMPTS AVAILABLE
+
+   password=fopen("pass.txt","r");
+   if(password==NULL){
+        password=fopen("pass.txt","a");
+        printf("\n  =================================== WELCOME TO PERSONAL DIARY MANAGEMENT SYSTEM ===================================\n\n");
+        printf("\n  Set a Password To Sign Up: ");
+        fgets(pass, sizeof(pass), stdin);
+        pass[strcspn(pass, "\n")] = '\0';
+        fprintf(password, "%s\n", pass);
+        fclose(password);
+    }
+   else {
+        fgets(pass, sizeof(pass), password);
+        pass[strcspn(pass, "\n")] = '\0';
+        fclose(password);
+    }
+
+  for(int i=1;i<=3;i++){
       system("cls || clear");// NEW PAGE
+      printf("\n  =================================== WELCOME TO PERSONAL DIARY MANAGEMENT SYSTEM ===================================\n\n");
       if(i!=1){
-        printf("\nWrong PIN! Try Again.\n");
+        printf("\n  Wrong PIN! Try Again.\n");
       }
-    printf("\nEnter PIN To Sign in: ");
+    printf("\n  Enter Your Password To Sign In: ");
     fgets(input_pass, sizeof(input_pass), stdin); // TAKING PIN AND CLEARING NEW LINE TO ENTER
     input_pass[strcspn(input_pass, "\n")] = '\0';
-    if(strcmp(input_pass,"CSE115")==0){
+    if(strcmp(input_pass,pass)==0){
     while (1) {
         system("cls || clear");
-        printf("\n======= MY Diary =======\n");
-        printf("\n1.Write a Note");
-        printf("\n2.Search and Read Notes");
-        printf("\n3.Exit");
-        printf("\n\nChoose One Number:");
+        printf("\n  ====================== MY Diary ======================\n");
+        printf("\n  1.Write a Note");
+        printf("\n  2.Search and Read Notes");
+        printf("\n  3.Exit");
+        printf("\n\n  Choose One Number:");
 
         choice = 0;
         scanf("%d", &choice);// TAKING USER CHOICE AND CLEARING THE EXTRA 'ENTER' FROM BUFFER
@@ -33,69 +51,72 @@ int main()
 
         case 1:
             system("cls || clear");
+            printf("\n  ====================== Write Notes ======================\n");
             fp = fopen("diary.txt", "a");// OPEN FILE IN 'APPEND' MODE TO ADD DATA WITHOUT DELETING OLD NOTES
 
-            printf("\nDate [DD/MM/YYYY]:");
+            printf("\n  Date [DD/MM/YYYY]:");
             fgets(date, sizeof(date), stdin);// fgets() CAPTURES THE WHOLE LINE INCLUDING SPACES
             date[strcspn(date, "\n")] = '\0';
 
-            printf("\nLocation:");
+            printf("\n  Location:");
             fgets(location, sizeof(location), stdin);
             location[strcspn(location, "\n")] = '\0';
 
-            printf("\nTopic:");
+            printf("\n  Topic:");
             fgets(subject, sizeof(subject), stdin);
             subject[strcspn(subject, "\n")] = '\0';
 
-            printf("\nEnter Note:");
+            printf("\n  Enter Note:");
             fgets(description, sizeof(description), stdin);
             description[strcspn(description, "\n")] = '\0';
 
             fprintf(fp, "%s | %s | %s | %s\n", date, location, subject, description);// WRITING DATA TO FILE WITH '|' SEPARATOR TO DISTINGUISH FIELDS LATER
             fclose(fp); // ALWAYS CLOSE THE FILE TO SAVE PROGRESS
-            printf("\nPress Enter to Continue.....");
+            printf("\n  Press Enter to Continue.....");
             getchar();
             break;
 
         case 2:
             system("cls || clear");
+            printf("\n  ====================== Search And Read ======================\n");
             fp = fopen("diary.txt", "r");// OPEN FILE IN 'READ' MODE
 
             if (fp == NULL) {
-                printf("\nDiary is empty.\n");
-                printf("\nPress Enter to Continue.....");
+                printf("\n  Diary is empty.\n");
+                printf("\n  Press Enter to Continue.....");
                 getchar();
                 break;
             }
 
-            printf("\nPress Enter to View All....");
-            printf("\n\nOr Enter Search Term: ");
+            printf("\n  Press Enter to View All....");
+            printf("\n\n  Or Enter Search Term: ");
             fgets(search, sizeof(search), stdin);// GETTING THE DATE FROM USER TO SEARCH
             search[strcspn(search, "\n")] = '\0';
 
             int found = 0; // READING EVERY LINE FROM FILE UNTILL THE END (EOF)
             while (fgets(line, sizeof(line), fp)) {// IF LINE CONTAINS THE SEARCHED STRING
                 if (strlen(search)==0 || strstr(line, search) != NULL) {// strstr() CHECKS IF THE SEARCH STRING EXISTS INSIDE THE CURRENT LINE
-                    printf("\n%s", line);// DISPLAYING THE MATCHING LINE
+                    printf("\n  %s", line);// DISPLAYING THE MATCHING LINE
                     found = 1;
                 }
             }
             if (found == 0)
-            printf("\nNot found!\n");
+            printf("\n  Not found!\n");
             fclose(fp);// CLOSING FILE AFTER READING
-            printf("\nPress Enter to Continue.....");
+            printf("\n  Press Enter to Continue.....");
             getchar();
             break;
 
         case 3:// TERMINATING THE INFINITE WHILE LOOP
             system("cls || clear");
-            printf("\nGoodbye!\n");
+            printf("\n  ====================== Exit ======================\n");
+            printf("\n  Goodbye!\n");
             return 0;
 
         default://WRONG NUMBER
             system("cls || clear");
-            printf("\nWrong Choice, Try Again\n");
-            printf("\nPress Enter to Continue.....");
+            printf("\n  Wrong Choice, Try Again\n");
+            printf("\n  Press Enter to Continue.....");
             getchar();
         }
     }
@@ -104,7 +125,8 @@ int main()
   }
   if(strcmp(input_pass,"CSE115")!=0) {
     system("cls || clear");
-    printf("\nWrong PIN! Closing program...\n");// WRONG PASSWORD
+    printf("\n  ====================== MY Diary ======================\n");
+    printf("\n  Wrong PIN! Closing program...\n");// WRONG PASSWORD
     return 0;
 }
 }
